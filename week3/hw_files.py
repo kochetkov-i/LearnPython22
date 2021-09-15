fed46afdfa818f757e1023193df84cc4b52ffef1
Kochetkov, Ilya <ilya.kochetkov@aliexpress.ru>
2021-09-15 22:10:52 +0300

    fix homework 3

--- week3/hw_files.py
+++ week3/hw_files.py
@@ -1,15 +1,19 @@
-len_file = 0
-count_words = 0
 filename = "referat.txt"
 new_filename = "referat2.txt"
-with open(filename, 'r', encoding='utf-8') as read_file:
-    for line in read_file:
-        len_file += len(line)
-        count_words += len(line.split())
-        with open(new_filename, 'a', encoding='utf-8') as add_file:
-            new_line = line.replace('.', '!')
-            add_file.writelines(new_line)
-    output_len_file = f"Длинна строки: {len_file}"
-    output_count_words = f"Количество слов: {count_words}"
-    print(output_len_file)
-    print(output_count_words)
+
+with open(filename, 'r', encoding='utf-8') as original_file:
+    content_original_file = original_file.read()
+
+content_new_file = content_original_file.replace('.', '!')
+
+with open(new_filename, 'w', encoding='utf-8') as new_file:
+    new_file.write(content_new_file)
+
+len_file = len(content_original_file)
+count_words = len(content_original_file.split())
+
+output_len_file = f"Длинна строки: {len_file}"
+output_count_words = f"Количество слов: {count_words}"
+
+print(output_len_file)
+print(output_count_words)
